ae5750234b84d78f9bf6c43767812bd706dc7980
Wu Mobius <energy908@gmail.com>
2020-10-31 15:07:10 +0800

    *-remove default migration and add bookkeeping migration

--- database/migrations/2020_10_31_065746_create_bookkeeping_table.php
+++ database/migrations/2020_10_31_065746_create_bookkeeping_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateBookkeepingTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('bookkeeping', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->enum('type', ['increase', 'decrease']);
+            $table->integer('amount');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('Bookkeeping');
     }
 }

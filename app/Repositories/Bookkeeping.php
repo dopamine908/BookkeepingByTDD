aeda884a6fdaad3207d953d4afc5744be229fa08
Wu Mobius <energy908@gmail.com>
2020-11-02 20:39:00 +0800

    [綠]-C-回傳成功-Repo-[撰寫功能]-在repo內寫新增的功能

---
+++ app/Repositories/Bookkeeping.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Models\Bookkeeping as BookkeepingModel;
+
+class Bookkeeping
+{
+    private $BookkeepingModel;
+
+    public function __construct(BookkeepingModel $BookkeepingModel)
+    {
+        $this->BookkeepingModel = $BookkeepingModel;
+    }
+
+    public function create($title, $type, $amount): bool
+    {
+        $Bookkeeping = new BookkeepingModel();
+        $Bookkeeping->title = $title;
+        $Bookkeeping->type = $type;
+        $Bookkeeping->amount = $amount;
+        return $Bookkeeping->save();
+    }
+}

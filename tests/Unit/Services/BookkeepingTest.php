2f450a6cea9afc0fc4e459ad82495420703a01f1
Wu Mobius <energy908@gmail.com>
2020-11-04 16:44:04 +0800

    [紅]-U-回傳成功-Service-[撰寫測試]-寫單元測試

--- tests/Unit/Services/BookkeepingTest.php
+++ tests/Unit/Services/BookkeepingTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Unit\Services;
 
+use App\Repositories\Bookkeeping;
 use App\Repositories\Bookkeeping as BookkeepingRepo;
 use App\Services\Bookkeeping as BookkeepingService;
+use Mockery;
 use Tests\TestCase;
 
 class BookkeepingTest extends TestCase
@@ -11,18 +13,42 @@ class BookkeepingTest extends TestCase
     /**
      * @test
      */
-    public function createBookkeeping_increase(){
+    public function createBookkeeping_increase()
+    {
         //Arrange
-        $mock_bookkeeping_repo=\Mockery::mock(BookkeepingRepo::class);
+        $mock_bookkeeping_repo = Mockery::mock(BookkeepingRepo::class);
         $mock_bookkeeping_repo->shouldReceive('create')->once()->andReturn(true);
-        $this->app->instance(BookkeepingRepo::class,$mock_bookkeeping_repo);
-        $BookkeepingService=$this->app->make(BookkeepingService::class);
+        $this->app->instance(BookkeepingRepo::class, $mock_bookkeeping_repo);
+        $BookkeepingService = $this->app->make(BookkeepingService::class);
         $title = 'test_title';
         $type = 'increase';
         $amount = 1234;
 
         //Actual
         $actual = $BookkeepingService->create($title, $type, $amount);
+
+        //Assert
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @test
+     */
+    public function updateBookkeeping_success()
+    {
+        //Arrange
+        $mock_bookkeeping_repo = Mockery::mock(BookkeepingRepo::class);
+        $mock_bookkeeping_repo->shouldReceive('update')->once()->andReturn(true);
+        $this->app->instance(BookkeepingRepo::class, $mock_bookkeeping_repo);
+        $BookkeepingService = $this->app->make(BookkeepingService::class);
+        $id = 1;
+        $title = 'new_title';
+        $type = 'increase';
+        $amount = 123456;
+
+        //Actual
+        $actual = $BookkeepingService->update($id, $title, $type, $amount);
+
 
         //Assert
         $this->assertTrue($actual);

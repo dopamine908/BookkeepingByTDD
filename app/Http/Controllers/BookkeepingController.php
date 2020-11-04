c19dacf87848f158a486184d6f154cceb9ae2b45
Wu Mobius <energy908@gmail.com>
2020-11-04 16:46:21 +0800

    [綠]-U-回傳成功-Service-[重構]-將controller功能串接到Service上

--- app/Http/Controllers/BookkeepingController.php
+++ app/Http/Controllers/BookkeepingController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Bookkeeping\Create;
-use App\Repositories\Bookkeeping as BookkeepingRepo;
 use App\Services\Bookkeeping as BookkeepingService;
 use Illuminate\Http\Request;
 
 class BookkeepingController extends Controller
 {
     private $BookkeepingService;
-    private $BookkeepingRepo;
 
-    public function __construct(BookkeepingService $BookkeepingService, BookkeepingRepo $BookkeepingRepo)
+    public function __construct(BookkeepingService $BookkeepingService)
     {
         $this->BookkeepingService = $BookkeepingService;
-        $this->BookkeepingRepo = $BookkeepingRepo;
     }
 
     public function create(Create $request)
@@ -26,7 +23,7 @@ class BookkeepingController extends Controller
 
     public function update(Request $request, $id)
     {
-        $this->BookkeepingRepo->update($id, $request->title, $request->type, $request->amount);
+        $this->BookkeepingService->update($id, $request->title, $request->type, $request->amount);
         return response()->json(['status' => 'success'], 201);
     }
 }

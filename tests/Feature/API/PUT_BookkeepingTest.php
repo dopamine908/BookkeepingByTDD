34b7740e2a1c4b9220b0dac816e19deaff345508
Wu Mobius <energy908@gmail.com>
2020-11-05 10:25:13 +0800

    [紅]-U-回傳失敗-API-[撰寫測試]-新增API測試(title為空)

--- tests/Feature/API/PUT_BookkeepingTest.php
+++ tests/Feature/API/PUT_BookkeepingTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\API;
 
 use App\Models\Bookkeeping;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class PUT_BookkeepingTest extends TestCase
@@ -38,4 +39,32 @@ class PUT_BookkeepingTest extends TestCase
         $this->assertDatabaseHas('Bookkeeping', $arrange_data);
         $this->assertDatabaseMissing('Bookkeeping', $original_data->toArray());
     }
+
+    /**
+     * @test
+     */
+    public function updateBookkeeping_title_empty_422()
+    {
+        //Arrange
+        $original_data = Bookkeeping::factory()->create();
+        $arrange_data = [
+            'type' => 'decrease',
+            'amount' => 12345
+        ];
+
+        //Actual
+        $response = $this->put(self::URL . '/' . $original_data->id, $arrange_data);
+
+        //Assert
+        $response->assertStatus(422);
+        $response->assertJson(
+            [
+                'status' => 'fail',
+                'message' => 'input invalid'
+            ]
+        );
+        $this->assertDatabaseMissing('Bookkeeping', $arrange_data);
+        $this->assertDatabaseHas('Bookkeeping', Arr::except($original_data->toArray(), ['updated_at', 'created_at']));
+    }
+
 }

296074dde15c37e0242cb250f31e147de04c28a4
Wu Mobius <energy908@gmail.com>
2020-11-05 14:03:15 +0800

    [紅]-U-回傳失敗-API-[撰寫測試]-新增API測試(id格式正確,但id不存在)

--- tests/Feature/API/PUT_BookkeepingTest.php
+++ tests/Feature/API/PUT_BookkeepingTest.php
@@ -227,4 +227,32 @@ class PUT_BookkeepingTest extends TestCase
         $this->assertDatabaseHas('Bookkeeping', Arr::except($original_data->toArray(), ['updated_at', 'created_at']));
     }
 
+
+    /**
+     * @test
+     */
+    public function updateBookkeeping_id_resource_not_exist_404(){
+        //Arrange
+        $original_data = Bookkeeping::factory()->create();
+        $not_exist_id=$original_data->id+99999;
+        $arrange_data = [
+            'title' => 'new_title',
+            'type' => 'decrease',
+            'amount' => 12345
+        ];
+
+        //Actual
+        $response = $this->put(self::URL . '/' . $not_exist_id, $arrange_data);
+
+        //Assert
+        $response->assertStatus(404);
+        $response->assertJson(
+            [
+                'status' => 'fail',
+                'message' => 'resource not found'
+            ]
+        );
+        $this->assertDatabaseMissing('Bookkeeping', $arrange_data);
+        $this->assertDatabaseHas('Bookkeeping', Arr::except($original_data->toArray(), ['updated_at', 'created_at']));
+    }
 }

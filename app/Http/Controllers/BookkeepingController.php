434559aa706a7ee54733805e4bbd14c02c7a96bc
Wu Mobius <energy908@gmail.com>
2020-11-05 14:04:25 +0800

    [綠]-U-回傳失敗-API-[撰寫功能]-在controller內過濾不存在資源的id

--- app/Http/Controllers/BookkeepingController.php
+++ app/Http/Controllers/BookkeepingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Bookkeeping\Create;
 use App\Http\Requests\Bookkeeping\Update;
+use App\Models\Bookkeeping as BookkeepingModel;
 use App\Services\Bookkeeping as BookkeepingService;
 
 class BookkeepingController extends Controller
@@ -23,7 +24,17 @@ class BookkeepingController extends Controller
 
     public function update(Update $request, $id)
     {
-        $this->BookkeepingService->update($id, $request->title, $request->type, $request->amount);
+        if (is_null(BookkeepingModel::find($id))) {
+            return response()->json(
+                [
+                    'status' => 'fail',
+                    'message' => 'resource not found'
+                ],
+                404
+            );
+        } else {
+            $this->BookkeepingService->update($id, $request->title, $request->type, $request->amount);
+        }
         return response()->json(['status' => 'success'], 201);
     }
 }

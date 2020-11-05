01cdfdcca6a73a656bbd89ba083b7a6f84bda450
Wu Mobius <energy908@gmail.com>
2020-11-05 11:58:43 +0800

    [紅]-U-回傳失敗-API-[撰寫測試]-新增API測試(route中的id為字串)

--- tests/Feature/API/PUT_BookkeepingTest.php
+++ tests/Feature/API/PUT_BookkeepingTest.php
@@ -199,4 +199,32 @@ class PUT_BookkeepingTest extends TestCase
         $this->assertDatabaseHas('Bookkeeping', Arr::except($original_data->toArray(), ['updated_at', 'created_at']));
     }
 
+    /**
+     * @test
+     */
+    public function updateBookkeeping_id_string_422()
+    {
+        //Arrange
+        $original_data = Bookkeeping::factory()->create();
+        $arrange_data = [
+            'title' => 'new_title',
+            'type' => 'increase',
+            'amount' => 123456
+        ];
+
+        //Actual
+        $response = $this->put(self::URL . '/string', $arrange_data);
+
+        //Assert
+        $response->assertStatus(422);
+        $response->assertJson(
+            [
+                'status' => 'fail',
+                'message' => 'input invalid'
+            ]
+        );
+        $this->assertDatabaseMissing('Bookkeeping', $arrange_data);
+        $this->assertDatabaseHas('Bookkeeping', Arr::except($original_data->toArray(), ['updated_at', 'created_at']));
+    }
+
 }

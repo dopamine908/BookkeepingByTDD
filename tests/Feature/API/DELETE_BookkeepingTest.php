592b22a052167ae3c95fecc2fd480867415d0317
Wu Mobius <energy908@gmail.com>
2020-11-05 15:38:15 +0800

    [綠]-D-回傳失敗-API-[撰寫測試]-新增API測試(id為空)

--- tests/Feature/API/DELETE_BookkeepingTest.php
+++ tests/Feature/API/DELETE_BookkeepingTest.php
@@ -36,4 +36,23 @@ class DELETE_BookkeepingTest extends TestCase
             Arr::except($original_data->toArray(), ['updated_at', 'created_at'])
         );
     }
+
+    /**
+     * @test
+     */
+    public function deleteBookkeeping_id_empty_405()
+    {
+        //Arrange
+        $original_data = Bookkeeping::factory()->create();
+
+        //Actual
+        $response = $this->delete(self::URL);
+
+        //Assert
+        $response->assertStatus(405);
+        $this->assertDatabaseHas(
+            'Bookkeeping',
+            Arr::except($original_data->toArray(), ['updated_at', 'created_at'])
+        );
+    }
 }
